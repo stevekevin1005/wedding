c12826404fb3ae42af3a3d439b897dbca0d5e4a0
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2024-03-05 14:44:20 +0800

    updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource/noto-sans-tc/400.css'
+import '@fontsource/noto-sans-tc/900.css'
 import './styles/global.scss'
 import App from './App'
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './style.scss'
 
 function Home() {
-    return <div>Home</div>
+    const video = useRef(null)
+
+    const handleWebSocket = () => {
+        const socket = new WebSocket('wss://party-line-bot.zeabur.app/api/v1/danmaku/ws')
+        socket.onopen = event => {
+            console.log('connect:', event)
+        }
+        socket.onmessage = event => {
+            console.log(event.data)
+        }
+    }
+
+    const handlePlayVideo = () => {
+        video.current.play()
+    }
+
+    useEffect(() => {
+        handleWebSocket()
+    }, [])
+
+    return (
+        <div className="home_wrapper">
+            <div className="bullet_screen">
+                <p className="text" data-stroke="測試 Sphinx of black quartz, judge my vow.">
+                    測試 Sphinx of black quartz, judge my vow.
+                </p>
+            </div>
+            <video className="video" ref={video} playsInline loop onClick={handlePlayVideo}>
+                <source src="https://cdn.chowsangsang.com/dfs/ivCssModelImages/92983/2022/1/14/a0e8f5e62a133e734edcd90c280601e1.mp4" />
+            </video>
+        </div>
+    )
 }
 
 export default Home
