3a3dc6ec65b7f6086909011f48db26fc409e4698
葉宗翰 <02090560@me.mcu.edu.tw>
2024-03-31 15:32:18 +0800

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
 function App() {
     return (
-        <div className="App">
-            <Home />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './style.scss'
 
 function Home() {
     const video = useRef(null)
+    const bulletElement = useRef(null)
+    const [texts, setTexts] = useState([])
 
     const handleWebSocket = () => {
         const socket = new WebSocket('wss://party-line-bot.zeabur.app/api/v1/danmaku/ws')
@@ -10,7 +12,10 @@ function Home() {
             console.log('connect:', event)
         }
         socket.onmessage = event => {
-            console.log(event.data)
+            const text = event.data
+            const array = [...texts]
+            array.push(text)
+            setTexts(array)
         }
     }
 
@@ -24,10 +29,12 @@ function Home() {
 
     return (
         <div className="home_wrapper">
-            <div className="bullet_screen">
-                <p className="text" data-stroke="測試 Sphinx of black quartz, judge my vow.">
-                    測試 Sphinx of black quartz, judge my vow.
-                </p>
+            <div className="bullet_screen" ref={bulletElement}>
+                {texts.map((text, index) => (
+                    <p key={index} className="text" data-stroke={text}>
+                        {text}
+                    </p>
+                ))}
             </div>
             <video className="video" ref={video} playsInline loop onClick={handlePlayVideo}>
                 <source src="https://cdn.chowsangsang.com/dfs/ivCssModelImages/92983/2022/1/14/a0e8f5e62a133e734edcd90c280601e1.mp4" />
