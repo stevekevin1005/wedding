a6c3aedb324ff5d2c19782222798e766b208743d
葉宗翰 <02090560@me.mcu.edu.tw>
2024-03-31 15:58:31 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import ImageList from './pages/ImageList'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/image-list" element={<ImageList />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/ImageList/index.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import './style.scss'
+
+function ImageList() {
+    const [list, setList] = useState([
+        {
+            id: 0,
+            name: 'string',
+            path: 'string'
+        }
+    ])
+
+    const getImageList = async () => {
+        try {
+            const response = await fetch('https://party-line-bot.zeabur.app/api/v1/images/list', { method: 'GET' })
+            if (response.ok) {
+                const result = await response.json()
+                setList(result)
+            }
+        } catch (error) {
+        } finally {
+        }
+    }
+
+    const handlePostMark = async id => {
+        try {
+            const object = { id }
+            const response = await fetch('https://party-line-bot.zeabur.app/api/v1/images/mark', {
+                method: 'POST',
+                body: JSON.stringify(object)
+            })
+            if (response.ok) {
+                // success evnet
+            } else {
+                const errorMessage = await response.text()
+                console.log('error message:', errorMessage)
+            }
+        } catch (error) {
+        } finally {
+        }
+    }
+
+    useEffect(() => {
+        getImageList()
+    }, [])
+
+    return (
+        <div className="image_list_wrapper">
+            <div className="list">
+                {list &&
+                    Array.isArray(list) &&
+                    list.map((data, index) => (
+                        <div
+                            key={index}
+                            className="image_box"
+                            onClick={() => {
+                                handlePostMark(data.id)
+                            }}>
+                            <div className="tag">已下載</div>
+                            <div className="image_wrap">
+                                <img src={data.path} />
+                            </div>
+                            <p>{data.name}</p>
+                        </div>
+                    ))}
+            </div>
+        </div>
+    )
+}
+
+export default ImageList
